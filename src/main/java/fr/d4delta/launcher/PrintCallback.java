1c01d58f06ae2833e33abfd2650b7b7dc47dc13c
D4Delta <D2Dead@gmail.com>
2015-07-01 12:49:17 +0200

    Changed license to LGPL

--- src/main/java/fr/d4delta/launcher/PrintCallback.java
+++ src/main/java/fr/d4delta/launcher/PrintCallback.java
@@ -1,17 +1,18 @@
 /* 
-This file is part of DeltaLauncher.
-DeltaLauncher is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+    This file is part of DeltaLauncher.
 
-DeltaLauncher is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    DeltaLauncher is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-You should have received a copy of the GNU General Public License
-along with DeltaLauncher.  If not, see <http://www.gnu.org/licenses/>.
+    DeltaLauncher is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the Lesser GNU General Public License
+    along with DeltaLauncher.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 package fr.d4delta.launcher;
